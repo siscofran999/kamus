e3f96b49860e6e134136c6f265e3468e60040c4d
siscofran <siscofran999@gmail.com>
2018-09-19 18:24:05 +0700

    dikit

--- app/src/main/java/com/example/tsmpc47/kamus/ui/main/MainActivity.java
+++ app/src/main/java/com/example/tsmpc47/kamus/ui/main/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.tsmpc47.kamus.ui.main;
 
+import android.arch.lifecycle.ViewModelProvider;
+import android.arch.lifecycle.ViewModelProviders;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
 
 import com.example.tsmpc47.kamus.BR;
 import com.example.tsmpc47.kamus.R;
@@ -14,11 +18,35 @@ import javax.inject.Inject;
 public class MainActivity extends BaseActivity<ActivityMainBinding, MainViewModel> implements MainNavigator {
 
     @Inject
-    MainViewModel mMainViewModel;
+    ViewModelProvider.Factory mViewModelFactory;
+
+    @Inject
+    LinearLayoutManager mLayoutManager;
+
+    private MainViewModel mMainViewModel;
+
+    private ActivityMainBinding mActivityMainBinding;
 
     public static Intent getStartMain(Context context){
         Intent intent = new Intent(context, MainActivity.class);
         return intent;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        mActivityMainBinding = getViewDataBinding();
+        mMainViewModel.setNavigator(this);
+
+        setUp();
+    }
+
+    private void setUp() {
+        mLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+
+        mActivityMainBinding.rc.setLayoutManager(mLayoutManager);
+        mActivityMainBinding.rc.setItemAnimator(new DefaultItemAnimator());
     }
 
     @Override
@@ -33,11 +61,7 @@ public class MainActivity extends BaseActivity<ActivityMainBinding, MainViewMode
 
     @Override
     public MainViewModel getViewModel() {
+        mMainViewModel = ViewModelProviders.of(this, mViewModelFactory).get(MainViewModel.class);
         return mMainViewModel;
-    }
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
     }
 }
--- app/src/main/java/com/example/tsmpc47/kamus/ui/main/MainActivityModule.java
+++ app/src/main/java/com/example/tsmpc47/kamus/ui/main/MainActivityModule.java
@@ -1,5 +1,9 @@
 package com.example.tsmpc47.kamus.ui.main;
 
+import android.arch.lifecycle.ViewModelProvider;
+import android.support.v7.widget.LinearLayoutManager;
+
+import com.example.tsmpc47.kamus.ViewModelProviderFactory;
 import com.example.tsmpc47.kamus.data.DataManager;
 import com.example.tsmpc47.kamus.ui.splash.SplashViewModel;
 import com.example.tsmpc47.kamus.utils.rx.SchedulerProvider;
@@ -13,5 +17,15 @@ public class MainActivityModule {
     @Provides
     MainViewModel provideMainViewModel(DataManager dataManager,SchedulerProvider schedulerProvider){
         return new MainViewModel(dataManager,schedulerProvider);
+    }
+
+    @Provides
+    LinearLayoutManager provideSearchLinearLayoutManager(MainActivity activity) {
+        return new LinearLayoutManager(activity);
+    }
+
+    @Provides
+    ViewModelProvider.Factory provideSearchViewModelProviderFactory(MainViewModel viewModel) {
+        return new ViewModelProviderFactory<>(viewModel);
     }
 }
